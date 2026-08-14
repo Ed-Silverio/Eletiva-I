35f93d2fc121e3d72ea1b7f6c13e744af78c0da6
Ed-Silverio <eduardo.silverio013@gmail.com>
2026-08-13 21:32:56 -0300

    Exercícios

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro cod em php</title>
+</head>
+<body>
+    <p>Ano: 2025</p>
+    <p>Ano: <?php echo date("Y"); ?></p>
+
+</body>
+</html>

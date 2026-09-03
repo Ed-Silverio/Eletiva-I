07fb34fcac77060ccbb512eef5e7b451e0a78e9d
Ed-Silverio <eduardo.silverio013@gmail.com>
2026-09-03 19:43:10 -0300

    Criação do exercício 6

---
+++ exercicio6.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ex6</title>
+</head>
+<body>
+<h1>Exercício 6</h1>
+<form method="post" action="resposta6.php">
+    <p>
+        <label for="tempF">Informe o primeiro valor:</label>
+        <input type="number" id="tempF" name="tempF">
+    </p>
+
+        <button type="submit">Enviar</button>
+        
+
+    </form>
+</body>
+</html>

dd17ff03a221134b9c31acc297f5db6d5318e8d6
Ed-Silverio <eduardo.silverio013@gmail.com>
2026-09-03 19:59:09 -0300

    Criação do exercício 8

---
+++ exercicio8.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EX8</title>
+</head>
+<body>
+<h1>Exercício 8</h1>
+<form method="post" action="resposta8.php">
+    <p>
+        <label for="raio">Informe o raio do círculo:</label>
+        <input type="number" id="raio" name="raio">
+    </p>
+
+        <button type="submit">Enviar</button>
+        
+
+    </form>
+    
+</body>
+</html>
---
+++ resposta8.php
@@ -0,0 +1,5 @@
+<?php
+    $r = $_POST["raio"];
+    $area = (3.14159 * ($r ** 2));
+
+    echo "A área do seu círculo é de $area";

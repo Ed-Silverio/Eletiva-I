e241a1b08660e12b55d286ca061728bfd5191e35
Ed-Silverio <eduardo.silverio013@gmail.com>
2026-09-03 20:41:05 -0300

    Correção do ex9 e criação do exercício 10

--- exercicio9.php
+++ exercicio9.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>EX7</title>
+    <title>EX9</title>
 </head>
 <body>
-<h1>Exercício 7</h1>
-<form method="post" action="resposta7.php">
+<h1>Exercício 9</h1>
+<form method="post" action="resposta9.php">
     <p>
         <label for="altura">Informe a altura do retângulo:</label>
         <input type="number" id="altura" name="altura">
---
+++ resposta10.php
@@ -0,0 +1,5 @@
+<?php
+    $r = $_POST["raio"];
+    $perimetro = (3.14159 * $r * 2);
+
+    echo "A área do seu círculo é de $perimetro";

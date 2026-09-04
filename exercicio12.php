aff171905a620def8e75155825d55432b2d46b7b
Ed-Silverio <eduardo.silverio013@gmail.com>
2026-09-03 21:20:49 -0300

    Criação do exercício 12

---
+++ exercicio12.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EX12</title>
+</head>
+<body>
+<h1>Exercício 12</h1>
+<form method="post" action="resposta12.php">
+    <p>
+        <label for="metro">Informe um valor em metros:</label>
+        <input type="number" id="metro" name="metro">
+    </p>
+
+        <button type="submit">Enviar</button>
+        
+
+    </form>
+    
+</body>
+</html>
---
+++ resposta12.php
@@ -0,0 +1,5 @@
+<?php
+    $m = $_POST['metro'];
+    $cm = ($m * 100);
+
+    echo "<p>$m metro(s) convertidos em centímetros, são $cm cm</p>";

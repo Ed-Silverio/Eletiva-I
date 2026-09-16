f7f4299ff307a8697b9672851c39cc5def1a0f4d
Ed-Silverio <eduardo.silverio013@gmail.com>
2026-09-15 22:30:46 -0300

    Correção da fórmula da resposta17.php

--- resposta17.php
+++ resposta17.php
@@ -2,6 +2,6 @@
     $capital = $_POST['capital'];
     $taxa = $_POST['taxa'];
     $periodo = $_POST['periodo'];
-    $juros = ($capital * (1 + $taxa/100)) ** $periodo;
+    $juros = ($capital * ((1 + $taxa/100) ** $periodo));
 
     echo "<p>O valor total é: $juros</p>";

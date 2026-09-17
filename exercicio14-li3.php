5ab48c2e269e481a66645e97b40b638e6e3536ea
Ed-Silverio <eduardo.silverio013@gmail.com>
2026-09-17 20:52:20 -0300

    Criação do exercício 14 da lista 3

---
+++ exercicio14-li3.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title></title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1></h1>
+<form method="post">
+<div class="mb-3">
+              <label for="word" class="form-label">Insira uma palavra</label>
+              <input type="text" id="word" name="word" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+
+<?php
+    if ($_POST) {
+    $word = $_POST['word'];
+    $invertida = strrev($word);
+    if ($word === $invertida) {
+        echo "<p>'$word' é um palíndromo.</p>";
+    } else {
+        echo "<p>'$word' não é um palíndromo.</p>";
+    }
+}
+?>
+
+</div>
+</body>
+</html>

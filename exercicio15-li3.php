c2d256957d81162c6d78e0c4eebabcf27944710e
Ed-Silverio <eduardo.silverio013@gmail.com>
2026-09-17 21:01:45 -0300

    Criação do exercício 15 da lista 3

---
+++ exercicio15-li3.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title></title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1></h1>
+<form method="post">
+<div class="mb-3">
+              <label for="email" class="form-label">Insira seu email</label>
+              <input type="text" id="email" name="email" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+
+<?php
+    if ($_POST) {
+    $email = $_POST['email'];
+    
+    //Usando a função explode() para dividir o email em 2 partes e pegando a segunda
+    $partes = explode('@', $email);
+    $dominio = $partes[1];
+
+    echo "<p>O domínio do email '$email' é: $dominio</p>";
+}
+?>
+
+</div>
+</body>
+</html>
